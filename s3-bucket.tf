b706459d9be64a4723f254638d88594de23504d6
Shashi Shankar <shashiishankar@gmail.com>
2021-08-19 19:44:16 +0000

    Added s3 code

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,7 +1,4 @@
 module "s3-bucket_example_complete" {
   source  = "app.terraform.io/shashis-training/s3-bucket/aws//examples/complete"
   version = "2.2.0"
-  bucket = "Asdsad"
-  bucket_prefix = "shshshi"
-
 }
